d5305ab1e48d10909eb76e8055974ab7706b032a
Jérémie Dumas <jeremie.dumas@ens-lyon.org>
2017-10-11 11:43:12 -0400

    Re-enable hints from env variables.

--- cmake/FindLIBIGL.cmake
+++ cmake/FindLIBIGL.cmake
@@ -9,10 +9,10 @@ endif()
 
 find_path(LIBIGL_INCLUDE_DIR igl/readOBJ.h
     HINTS
-        # ENV LIBIGL
-        # ENV LIBIGLROOT
-        # ENV LIBIGL_ROOT
-        # ENV LIBIGL_DIR
+        ENV LIBIGL
+        ENV LIBIGLROOT
+        ENV LIBIGL_ROOT
+        ENV LIBIGL_DIR
     PATHS
         ${CMAKE_SOURCE_DIR}/../..
         ${CMAKE_SOURCE_DIR}/..
