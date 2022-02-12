cb41e6281cfebfab0617b585a9c370ac247f9e89
Alec Jacobson <alecjacobson@gmail.com>
2022-02-12 11:43:56 -0500

    bump libigl ; rm _WITH in cmake option

--- cmake/libigl.cmake
+++ cmake/libigl.cmake
@@ -6,6 +6,6 @@ include(FetchContent)
 FetchContent_Declare(
     libigl
     GIT_REPOSITORY https://github.com/libigl/libigl.git
-    GIT_TAG b0fd49d59840b5a48e79096462d3198a5c13eee9
+    GIT_TAG 10b3cd442e69e0a4e0287387453f7f0bf60ae4b2
 )
 FetchContent_MakeAvailable(libigl)
