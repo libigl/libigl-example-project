a9b087f9d8e9cce32ab68705128164cd5d4ad81a
Alec Jacobson <alecjacobson@gmail.com>
2022-02-19 16:42:41 -0500

    use release tag

--- cmake/libigl.cmake
+++ cmake/libigl.cmake
@@ -6,6 +6,6 @@ include(FetchContent)
 FetchContent_Declare(
     libigl
     GIT_REPOSITORY https://github.com/libigl/libigl.git
-    GIT_TAG 10b3cd442e69e0a4e0287387453f7f0bf60ae4b2
+    GIT_TAG v2.4.0
 )
 FetchContent_MakeAvailable(libigl)
