468c262a9aebb4e8c70ec9891f5a7a7d13cbfbaf
Alec Jacobson <alecjacobson@gmail.com>
2022-03-07 11:18:07 -0500

    cgal example

--- main.cpp
+++ main.cpp
@@ -1,30 +1,16 @@
 #include <igl/opengl/glfw/Viewer.h>
+#include <igl/copyleft/cgal/convex_hull.h>
 
 int main(int argc, char *argv[])
 {
   // Inline mesh of a cube
-  const Eigen::MatrixXd V= (Eigen::MatrixXd(8,3)<<
+  const Eigen::MatrixXd V= (Eigen::MatrixXd(4,3)<<
     0.0,0.0,0.0,
     0.0,0.0,1.0,
     0.0,1.0,0.0,
-    0.0,1.0,1.0,
-    1.0,0.0,0.0,
-    1.0,0.0,1.0,
-    1.0,1.0,0.0,
-    1.0,1.0,1.0).finished();
-  const Eigen::MatrixXi F = (Eigen::MatrixXi(12,3)<<
-    1,7,5,
-    1,3,7,
-    1,4,3,
-    1,2,4,
-    3,8,7,
-    3,4,8,
-    5,7,8,
-    5,8,6,
-    1,5,6,
-    1,6,2,
-    2,6,8,
-    2,8,4).finished().array()-1;
+    1.0,0.0,0.0).finished();
+  Eigen::MatrixXi F;
+  igl::copyleft::cgal::convex_hull(V,F);
 
   // Plot the mesh
   igl::opengl::glfw::Viewer viewer;
